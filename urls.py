834e3e96c9385f51abf842a476250edb55050506
sowmya755 <sowmya4301@gmail.com>
2026-04-02 12:46:17 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,19 +1,23 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for passmanager project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home_page, name='home'),  # Home page
-    path("signup/", views.signup_page, name="signup"),
-    path("login/", views.login_page, name="login"),
-    path("dashboard/", views.dashboard, name="dashboard"),
-     path("enter-pin/<int:pk>/", views.enter_pin, name='enter_pin'),
-    path("vault/<int:pk>/", views.vault_views, name='vault'),
-    path("add/", views.add_password, name="add_password"),
-    path("logout/", views.logout_user, name="logout"),
-    path("update/<int:pk>/", views.update_password, name="update_password"),
-    path("delete/<int:pk>/", views.delete_password, name="delete_password"),
-    path('help/', views.help_page, name='help')
-    
-    
-
+    path('admin/', admin.site.urls),
+    path('', include('secure.urls')),
 ]
